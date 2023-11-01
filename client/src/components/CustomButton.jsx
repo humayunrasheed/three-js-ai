4e17a751fe7e43a69458849fe86a89b7547801bc
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-01 15:14:42 +0530

    Home Page created

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,26 @@
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import React from 'react'
+
+
+const CustomButton = ({type,title,customStyles,handleClick}) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if(type ==='filed'){
+      return{
+        backgroundColor: snap.color,
+        color :'#fff'
+      }
+    }
+  }
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style = {generateStyle(type)}
+      onClick={handleClick}>
+        {title}
+    </button>
+  )
+}
+
+export default CustomButton
