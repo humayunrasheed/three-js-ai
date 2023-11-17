2721d8fb9e20e62891923b4bbbdb5a17ce8e0e0d
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-17 15:56:35 +0530

    solve small bug

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,32 +1,33 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import OpenAI from "openai";
-
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
 dotenv.config();
+
 const router = express.Router();
 
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-});
-router.route('/').post(async(req , res)=>{
-    try {
-        const {prompt} = req.body
-        const response = await openai.createImage({
-            prompt,
-            n:1,
-            size:'1024x1024',
-            response_format:'b64_json'
-        });
-        const image = response.data.data[0].b64_json;
-        res.status(200).json({photo : image})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({message :"Something went wrong"})
-    }
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.images.generate({ model: "dall-e-2",prompt, n: 1, size: "1024x1024",response_format: 'b64_json' });
+    // const response = await openai.images({
+    //   model: "dall-e-3",
+    //   prompt,
+    //   n: 1,
+    //   size: "1024x1024",
+    //   response_format: 'b64_json'
+    // });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }  
 })
-router.route('/').get((req,res)=>{
-    res.status(200).json({message:"Hello from Dalle Routes"})
-})
+
 export default router;
