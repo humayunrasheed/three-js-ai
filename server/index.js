b257e70b2d1dd5b439bb25bf11711491ccb935e6
humayunrasheed <104017282+humayunrasheed@users.noreply.github.com>
2023-11-17 19:11:32 +0530

    solving error

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-// index.js
 import express from 'express';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import dalleRoutes from './routes/dalle.routes.js';
 
@@ -9,12 +8,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limig: "50mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E" });
-});
+  res.status(200).json({ message: "Hello from DALL.E" })
+})
 
-app.listen(8080, () => console.log('Server has started on port 8080'));
+app.listen(8080, () => console.log('Server has started on port 8080'))
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import OpenAI from 'openai';
+import { Configuration, OpenAIApi} from 'openai';
+
 dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -14,20 +19,21 @@ router.route('/').get((req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const response = await openai.images.generate({ model: "dall-e-2",prompt, n: 1, size: "1024x1024",response_format: 'b64_json' });
-    // const response = await openai.images({
-    //   model: "dall-e-3",
-    //   prompt,
-    //   n: 1,
-    //   size: "1024x1024",
-    //   response_format: 'b64_json'
-    // });
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    });
+
     const image = response.data.data[0].b64_json;
+
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal Server Error", error: error.message });
-  }  
+    res.status(500).json({ message: "Something went wrong" })
+  }
 })
 
 export default router;
